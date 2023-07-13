3a6f853cd830ddd7bdbda9449fb22d815ce5b46d
Intern9 <intern9@Intern9s-MacBook-Air.local>
2023-07-13 14:30:28 +0800

    added textstyle

---
+++ lib/sample.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class SampleScreen extends StatefulWidget {
+  const SampleScreen({super.key});
+
+  @override
+  State<SampleScreen> createState() => _SampleScreenState();
+}
+
+class _SampleScreenState extends State<SampleScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Text("sample"),
+          )
+        ],
+      ),
+    );
+  }
+}
